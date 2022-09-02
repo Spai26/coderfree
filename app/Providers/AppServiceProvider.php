7fabb9e48003b506181a25ec049c0560cd196bd1
SergioAi <sergioai.si93@gmail.com>
2022-09-01 19:54:15 -0500

    configuracion picsum para faker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!$this->app->environment('production')) {
+            $this->app->register('App\Providers\FakerServiceProvider');
     }
 }
